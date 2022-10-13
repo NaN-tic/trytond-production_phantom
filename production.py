383105314638b1c3447d8b831512a48e45c61fe6
Àngel Àlvarez <angel@nan-tic.com>
2022-10-13 10:58:29 +0200

    adapt set_moves to new signature

--- production.py
+++ production.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 from trytond.pool import PoolMeta, Pool
-
+from trytond.model import dualmethod
 
 class Production(metaclass=PoolMeta):
     __name__ = 'production'
@@ -33,43 +33,48 @@ class Production(metaclass=PoolMeta):
                         new_inputs.append(move)
             self.inputs = new_inputs
 
-    def set_moves(self):
+
+    @dualmethod
+    def set_moves(cls, productions):
         Move = Pool().get('stock.move')
         Product = Pool().get('product.product')
         Template = Pool().get('product.template')
 
-        super(Production, self).set_moves()
+        super(Production, cls).set_moves(productions)
         to_delete = []
-        cost = Decimal(0)
-        for move in self.inputs:
-            if not move.product.phantom or not move.product.boms:
-                cost += (Decimal(str(move.internal_quantity)) *
-                    move.product.cost_price)
-                continue
-            to_delete.append(move)
-            bom = move.product.boms[0].bom
-            factor = bom.compute_factor(move.product, move.quantity, move.uom)
-            for input_ in bom.inputs:
-                quantity = input_.compute_quantity(factor)
-                product = input_.product
-                new_move = self._move(move.from_location, move.to_location,
-                    move.company, product, input_.uom, quantity)
-                if new_move:
-                    new_move.production_input = self
-                    new_move.planned_date = self.planned_date
-                    # TODO: This should be outside the loop
-                    new_move.save()
-                    cost += Decimal(str(quantity)) * product.cost_price
-        if hasattr(Product, 'cost_price'):
-            digits = Product.cost_price.digits
-        else:
-            digits = Template.cost_price.digits
-        if to_delete:
-            for output in self.outputs:
-                if move.product == self.product:
-                    move.unit_price = Decimal(
-                        cost / Decimal(str(output.internal_quantity))
-                        ).quantize(Decimal(str(10 ** -digits[1])))
-                    move.save()
-            # TODO: We still miss a way to invalidate the cache of self
-            Move.delete(to_delete)
+        for production in productions:
+            cost = Decimal(0)
+            for move in production.inputs:
+                if not move.product.phantom or not move.product.boms:
+                    cost += (Decimal(str(move.internal_quantity)) *
+                        move.product.cost_price)
+                    continue
+                to_delete.append(move)
+                bom = move.product.boms[0].bom
+                factor = bom.compute_factor(move.product, move.quantity,
+                    move.uom)
+                for input_ in bom.inputs:
+                    quantity = input_.compute_quantity(factor)
+                    product = input_.product
+                    new_move = production._move(move.from_location,
+                            move.to_location, move.company, product,
+                            input_.uom, quantity)
+                    if new_move:
+                        new_move.production_input = production
+                        new_move.planned_date = production.planned_date
+                        # TODO: This should be outside the loop
+                        new_move.save()
+                        cost += Decimal(str(quantity)) * product.cost_price
+            if hasattr(Product, 'cost_price'):
+                digits = Product.cost_price.digits
+            else:
+                digits = Template.cost_price.digits
+            if to_delete:
+                for output in production.outputs:
+                    if move.product == production.product:
+                        move.unit_price = Decimal(
+                            cost / Decimal(str(output.internal_quantity))
+                            ).quantize(Decimal(str(10 ** -digits[1])))
+                        move.save()
+                # TODO: We still miss a way to invalidate the cache of self
+                Move.delete(to_delete)
